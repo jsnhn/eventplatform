578b7c40fbbf01e4170aa9732fca1762b799a3d8
jsnhn <jayhh122@gmail.com>
2024-06-26 18:44:17 -0700

    adds index page

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
+events = [
+  {'name': 'Lolo'}
+]
+
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def events_index(request):
+    return render(request, 'events/index.html', {
+        'events': events
+    })
